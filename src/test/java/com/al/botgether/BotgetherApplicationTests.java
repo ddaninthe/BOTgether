5a19ff4ee489b9b719207f873da1e2cee3a44a3f
ddaninthe <d.daninthelopez@gmail.com>
2019-04-11 17:04:25 +0200

    First commit

---
+++ src/test/java/com/al/botgether/BotgetherApplicationTests.java
@@ -0,0 +1,16 @@
+package com.al.botgether;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BotgetherApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
