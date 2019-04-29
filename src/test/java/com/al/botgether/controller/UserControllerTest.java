27801d84bcbd711480f9d5a58ba05c13359e6887
ddaninthe <d.daninthelopez@gmail.com>
2019-04-29 23:26:36 +0200

    Fixed compilation issue

--- src/test/java/com/al/botgether/controller/UserControllerTest.java
+++ src/test/java/com/al/botgether/controller/UserControllerTest.java
@@ -4,8 +4,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest
+//@RunWith(SpringRunner.class)
 public class UserControllerTest {
     // TODO
 }
