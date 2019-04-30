875615496cce6eb598ba9bd4c8dbf6c4a18e472d
ddaninthe <d.daninthelopez@gmail.com>
2019-04-30 14:49:29 +0200

    UserController first Test

--- src/main/java/com/al/botgether/controller/UserController.java
+++ src/main/java/com/al/botgether/controller/UserController.java
@@ -3,13 +3,15 @@ package com.al.botgether.controller;
 import com.al.botgether.dto.UserDto;
 import com.al.botgether.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping(value = "/users")
 public class UserController {
 
     private final UserService userService;
@@ -21,19 +23,22 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity getUser(@PathVariable("id") String userId) {
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+
         if (userId.matches("^\\d+$")) {
             UserDto userDto = userService.getById(userId);
             if (userDto == null) { // Not found
-                return ResponseEntity.notFound().build();
+                return ResponseEntity.notFound().headers(headers).build();
             } else {
-                return ResponseEntity.ok(userDto);
+                return ResponseEntity.ok().headers(headers).body(userDto);
             }
         } else {
             List<UserDto> usersDto = userService.getUserByUsername(userId);
             if (usersDto.isEmpty()) { // Not found
-                return ResponseEntity.notFound().build();
+                return ResponseEntity.notFound().headers(headers).build();
             } else {
-                return ResponseEntity.ok(usersDto.get(0));
+                return ResponseEntity.ok().headers(headers).body(usersDto.get(0));
             }
         }
     }
--- src/test/java/com/al/botgether/controller/UserControllerTest.java
+++ src/test/java/com/al/botgether/controller/UserControllerTest.java
@@ -1,10 +1,36 @@
 package com.al.botgether.controller;
 
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(SpringRunner.class)
+import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.RestAssured.when;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 public class UserControllerTest {
-    // TODO
+
+    @LocalServerPort
+    private int port;
+
+    @Before
+    public void setup() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    public void should_return_not_found() {
+        when()
+            .get("/users/{id}", "02")
+        .then()
+            .statusCode(404);
+    }
 }
