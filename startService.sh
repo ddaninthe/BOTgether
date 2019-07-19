7722762cc358697a45cccd0ae2f34f9059398e33
Thomas Gianfalla <gianfalla.t@gmail.com>
2019-07-19 18:41:58 +0200

    Update startService.sh

--- startService.sh
+++ startService.sh
@@ -1,2 +1,7 @@
-cd /home/eubuntu/BOTgether
+cd /home/ubuntu/BOTgether
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum -y install -y apache-maven
+sudo yum -y install java-1.8.0-openjdk-devel
+export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk.x86_64
 mvn spring-boot:run
