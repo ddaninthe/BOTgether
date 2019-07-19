a1fd467c06309c09c0b3d0f679e4cca5baa87351
Thomas Gianfalla <gianfalla.t@gmail.com>
2019-07-19 21:54:03 +0200

    Update startService.sh

--- startService.sh
+++ startService.sh
@@ -3,5 +3,5 @@ sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-mav
 sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
 sudo yum -y install -y apache-maven
 sudo yum -y install java-1.8.0-openjdk-devel
-export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk.x86_64
-mvn spring-boot:run
+sudo export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk.x86_64
+sudo mvn spring-boot:run
